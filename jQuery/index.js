3baf8a71baa69d4717aff91a9f28e9c00c91f35f
Kmart914 <kmart914@gmail.com>
2020-12-15 12:24:45 -0800

    Added fils for Simon game and linked JS and CSS. Also added H1

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,13 @@
-$("h1").addClass("bigTitle margin50");
 
-$("button").html("Don't click me");
+
+$("body").keypress(function(event){
+  console.log(event.key);
+  $("h1").html(event.key);
+})
+
+$("body").on("mouseover", function(){
+  $("h1").css("color", "purple")
+});
+// $("button").click(function(){
+//   $("h1").css("color", "red");
+// });

01ee8d3aa3ba341704357217872421684b4999ec
Kmart914 <kmart914@gmail.com>
2020-12-19 21:19:45 -0800

    finished basic functionality of calculator

--- ExpressCalculator/server.js
+++ ExpressCalculator/server.js
@@ -1,17 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/',function(req,res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile("/index.html", { root : __dirname});
 });
+
+
+
+
+app.post("/", function(req, res) {
+
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+
+  res.send("The Total is " + result);
+
+});
+
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
-
-
-
-//
-// var firstNumber = document.getElementById("#firstNumber").value;
-// var secondNumber = document.getElementById("#secondNumber").value;

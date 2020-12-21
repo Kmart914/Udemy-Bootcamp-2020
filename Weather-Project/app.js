7bbdebc870e0d34da52075be1860bab90bf29db3
Kmart914 <kmart914@gmail.com>
2020-12-21 11:25:39 -0800

    Started Weather API Project

---
+++ Weather-Project/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const https = require("https");
+
+
+
+const app = express();
+
+app.get("/", function(req, res){
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Irvine&appid=a7388ba3bc8873b5fa1c9709c90dfa20"
+  https.get(url, function(response){
+    console.log(response.statusCode);
+  })
+  res.send("Server is running");
+})
+
+app.listen(3000, function(){
+  console.log("Server is running on 3000");
+})

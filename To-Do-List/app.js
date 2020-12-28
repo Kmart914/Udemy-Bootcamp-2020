8bb5254e3b933f97e06a17676edad6433d52a5b4
kevin <kmart914@gmail.com>
2020-12-28 13:03:14 -0800

    Added logic to pull date and place on header card using EJS

--- To-Do-List/app.js
+++ To-Do-List/app.js
@@ -7,14 +7,37 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+var weekday = new Date();
+
+var day = daysOfWeek[weekday.getDay()];
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+const d = new Date();
+
+var currentMonth = monthNames[d.getMonth()];
+
+var dayOfMonth = new Date().getDate();
+
+var dateDisplay = day + ", " + currentMonth + " " + dayOfMonth;
+
+app.set('view engine', 'ejs');
+
+
 app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
-  res.sendFile("/index.html", { root : __dirname});
+  res.render('list', {kindOfDay: dateDisplay});
 
 });
 
   app.listen(3000, function(){
     console.log("Server is running on 3000");
+    console.log(day);
+    console.log(currentMonth);
   });

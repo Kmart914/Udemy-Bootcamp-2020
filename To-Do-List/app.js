2d342615481fbd9079d7ddf7523cd8e66f997c06
kevin <kmart914@gmail.com>
2020-12-29 10:56:31 -0800

    Added EJS logic to add task onto page after being added in text box.

--- To-Do-List/app.js
+++ To-Do-List/app.js
@@ -7,37 +7,45 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-var weekday = new Date();
-
-var day = daysOfWeek[weekday.getDay()];
-
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-
-const d = new Date();
-
-var currentMonth = monthNames[d.getMonth()];
-
-var dayOfMonth = new Date().getDate();
-
-var dateDisplay = day + ", " + currentMonth + " " + dayOfMonth;
-
-app.set('view engine', 'ejs');
-
+var addTasks = [];
 
 app.use(express.static("public"));
 
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+var today = new Date();
+
+var options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+};
+
+
+var day = today.toLocaleDateString("en-us", options)
+
+
+
+
 app.get("/", function(req, res){
-  res.render('list', {kindOfDay: dateDisplay});
+  res.render('list', {kindOfDay: day, addedTasks: addTasks});
 
 });
+
+app.post("/", function(req, res) {
+  var addTask = req.body.newTask;
+
+  addTasks.push(addTask);
+  res.redirect("/");
+
+})
+
+
 
   app.listen(3000, function(){
     console.log("Server is running on 3000");
     console.log(day);
-    console.log(currentMonth);
   });
